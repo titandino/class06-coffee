dac3f8936fd1dc22cadfdd1c470056060a9fc437
Trenton Kress <trenton.kress@gmail.com>
2016-08-09 21:17:41 -0700

    literals started

---
+++ app.js
@@ -0,0 +1,6 @@
+var locations = [];
+locations.push({ locName: 'Pike Place Market', minCust: minCust, maxCust: maxCust, avgCups: avgCups, avgLbs: avgLbs });
+locations.push({ locName: 'Capitol Hill', minCust: minCust, maxCust: maxCust, avgCups: avgCups, avgLbs: avgLbs });
+locations.push({ locName: 'Seattle Public Library', minCust: minCust, maxCust: maxCust, avgCups: avgCups, avgLbs: avgLbs });
+locations.push({ locName: 'South Lake Union', minCust: minCust, maxCust: maxCust, avgCups: avgCups, avgLbs: avgLbs });
+locations.push({ locName: 'Sea-Tac Airport', minCust: minCust, maxCust: maxCust, avgCups: avgCups, avgLbs: avgLbs });

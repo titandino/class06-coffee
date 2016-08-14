e977e94abb3a0c3a83adb46d13e526905a8b035a
Trenton Kress <trenton.kress@gmail.com>
2016-08-14 09:42:39 -0700

    That's a semicolon that shouldn't be there!

--- app.js
+++ app.js
@@ -117,7 +117,7 @@ function randInt(min, max) {
 
 function round(number, dec) {
   return Math.round(number * dec) / dec;
-};
+}
 
 function hourToTime(hour) {
   var hours = [6, 7, 8, 9, 10, 11, 12, 1, 2, 3, 4, 5, 6, 7, 8, 9];
